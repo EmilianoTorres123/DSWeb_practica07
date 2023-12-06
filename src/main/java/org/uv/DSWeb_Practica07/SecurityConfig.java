821baac9fef204d4da626e7b238dd6dde6662f24
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-12-06 10:38:06 -0600

    ADD Security Config

--- src/main/java/org/uv/DSWeb_Practica07/SecurityConfig.java
+++ src/main/java/org/uv/DSWeb_Practica07/SecurityConfig.java
@@ -4,13 +4,53 @@
  */
 package org.uv.DSWeb_Practica07;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-//tenemos que investigaer para que funciona el securityconfig
 /**
  *
  * @author zurisaddairj
  */
-
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
- 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeHttpRequests().mvcMatchers("/api/User").permitAll()
+                .anyRequest().authenticated().and().httpBasic();
+//                .authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+
+        return http.build();
+    }
+    
+     @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder ();
+
+        return passwordEncoder;
+    }
+    
+     @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user1= org.springframework.security.core.userdetails.User
+                .withUsername("zuri")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+        
+        UserDetails user2= org.springframework.security.core.userdetails.User
+                .withUsername("damian")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
